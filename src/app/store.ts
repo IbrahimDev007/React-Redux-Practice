b3d0cdc260555e4ed01358b4c744aa16562a30da
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-10-30 09:36:31 +0600

    intially provider and store setup export store dype and dispatch for fiuture checking

---
+++ src/app/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+
+
+export const store=configureStore(
+
+{
+    reducer: {}
+}
+
+);
+
+export type RootState=ReturnType<typeof  store.getState >;
+
+export type AppDispatcher=ReturnType<typeof store.dispatch>;
