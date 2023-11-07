b8be53ebe0d1a9b6d7718eed544606968db81abc
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-11-07 23:42:41 +0600

    tailwind css added for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { Counters } from "./Fetures/Counter/Counters";
+import { Counter } from "./Fetures/Counter/Counters";
 
 function App() {
 	return (
-		<>
-			<Counters />
-		</>
+		<div className="grid grid-cols-2 md:lg:xl:grid-cols-3">
+			<Counter />
+		</div>
 	);
 }
 
--- src/Fetures/Counter/Counters.tsx
+++ src/Fetures/Counter/Counters.tsx
@@ -2,27 +2,29 @@ import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../../app/store";
 import { decrement, increment } from "./slice";
 
-export function Counters() {
+export function Counter() {
 	const count = useSelector((state: RootState) => state.counter.value);
 	const dispatch = useDispatch();
 
 	return (
-		<div className="bg-gradient-to-b from-blue-500 to-purple-500 h-screen flex justify-center items-center">
-			<div className="bg-white rounded-lg p-4 shadow-lg">
-				<div className="text-3xl text-gray-800 font-bold text-center mb-4">
-					Counter Value: {count}
-				</div>
-				<div className="flex justify-center space-x-4">
+		<div className="card w-96 glass">
+			<div className="card-body">
+				<h2 className="card-title my-2">
+					count <span className="mx-3 badge badge-secondary">{count}</span>
+				</h2>
+				<p>
+					Here is two state increment and decrement <br />
+					just click and show result
+				</p>
+				<div className="card-actions justify-between mt-2">
 					<button
-						className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-full focus:outline-none"
-						aria-label="Increment value"
+						className="btn btn-success"
 						onClick={() => dispatch(increment())}
 					>
 						Increment
 					</button>
 					<button
-						className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-full focus:outline-none"
-						aria-label="Decrement value"
+						className="btn btn-error"
 						onClick={() => dispatch(decrement())}
 					>
 						Decrement
