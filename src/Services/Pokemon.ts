e119d5a89e4ce7b852ba0548bfa111b81847b774
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-11-09 00:08:18 +0600

    create api and store setup and midalwar

---
+++ src/Services/Pokemon.ts
@@ -0,0 +1,14 @@
+// rtk query api create
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+type Pokemon = { query: string };
+
+export const pokemonAPI = createApi({
+	reducerPath: "pokemonApi",
+	baseQuery: fetchBaseQuery({ baseUrl: "https://pokeapi.co/api/v2/" }),
+	endpoints: (builder) => ({
+		getPokeonByName: builder.query<Pokemon, string>({
+			query: (name: string) => `pokemon/${name}`,
+		}),
+	}),
+});
+export const { useGetPokeonByNameQuery } = pokemonAPI;
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../Fetures/Counter/slice";
+import { pokemonAPI } from "../Services/Pokemon";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
 	reducer: {
 		counter: counterReducer,
+		[pokemonAPI.reducerPath]: pokemonAPI.reducer,
+	},
+	middleware: (getDefaultMiddleware) => {
+		return getDefaultMiddleware().concat(pokemonAPI.middleware);
 	},
 });
+setupListeners(store.dispatch);
 
 export type RootState = ReturnType<typeof store.getState>;
 
