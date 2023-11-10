0398e01e598d9aee56050a458dca41bdf0c636db
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-11-10 12:32:32 +0600

    custom midalware add and api change to js to ts

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,18 +2,25 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../Fetures/Counter/slice";
 import { pokemonAPI } from "../Services/Pokemon";
 import { setupListeners } from "@reduxjs/toolkit/query";
+import { postApi } from "../Services/Post";
+
+const pokemonMiddleware = pokemonAPI.middleware;
+const postMiddleware = postApi.middleware;
 
 export const store = configureStore({
 	reducer: {
 		counter: counterReducer,
 		[pokemonAPI.reducerPath]: pokemonAPI.reducer,
+		[postApi.reducerPath]: postApi.reducer,
 	},
 	middleware: (getDefaultMiddleware) => {
-		return getDefaultMiddleware().concat(pokemonAPI.middleware);
+		const defaultMiddleware = getDefaultMiddleware();
+		const PokemonMiddleware = [...defaultMiddleware, pokemonMiddleware];
+		const PostMiddleware = [...defaultMiddleware, postMiddleware];
+		return [...PokemonMiddleware, ...PostMiddleware];
 	},
 });
 setupListeners(store.dispatch);
 
 export type RootState = ReturnType<typeof store.getState>;
-
 export type AppDispatcher = ReturnType<typeof store.dispatch>;
